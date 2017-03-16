4c0391b74a9d970ecf125ad3713baec4043f4ede
Timothy Gonzalez <timg456789@yahoo.com>
2017-03-16 02:16:29 -0400

    journal with the ability to save. diff is performed with save button in top right to show red when a save is needed after adding or removing text, then green after successfully saving. Every save attempt will save to a new file named by the date in UTC.

--- src/home.js
+++ src/home.js
@@ -3,18 +3,47 @@ function Home() {
 
     this.init = function () {
 
+        $('#input').keypress(function() {
+            $('#save').addClass('btn-danger');
+            $('#save').removeClass('btn-success');
+        });
+
         $('#save').click(function () {
 
             const UrlParameter = require('url-parameter/src/url-parameter');
             var urlParameter = new UrlParameter();
             var url = window.location.href;
 
-            var accessKeyId = urlParameter.getParameterByName(url, 'AccessKeyId');
-            var secretAccessKey = urlParameter.getParameterByName(url, 'SecretAccessKey');
+            var params = {};
+            params.accessKeyId = urlParameter.getParameterByName(url, 'AccessKeyId');
+            params.secretAccessKey = urlParameter.getParameterByName(url, 'SecretAccessKey');
+            params.bucket = urlParameter.getParameterByName(url, 'Bucket');
 
+            var AWS = require('aws-sdk');
+            AWS.config.update(
+                {
+                    accessKeyId: params.accessKeyId,
+                    secretAccessKey: params.secretAccessKey,
+                    region: 'us-east-1'
+                }
+            );
+            var s3 = new AWS.S3();
 
-            console.log(accessKeyId);
-            console.log(secretAccessKey);
+            var options = {};
+            options.Bucket = params.bucket;
+            options.Key = new Date().toISOString();
+            options.Body = $('#input').val().trim();
+
+            s3.upload(options, function (err, response) {
+                if (err) {
+                    console.log('failure saving settings: ' + JSON.stringify(err, 0, 4));
+                } else {
+                    $('#save').addClass('btn-success');
+                    $('#save').removeClass('btn-danger');
+                    console.log(JSON.stringify(response));
+                }
+
+            });
 
         });
     };
